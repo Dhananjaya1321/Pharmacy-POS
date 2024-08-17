a689a629eae3d3a4c1002887ee60a5d4738da95f
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2024-08-17 16:02:28 +0530

    Created entities

---
+++ src/main/java/com/novalabsglobal/pharmacy/entity/Item.java
@@ -0,0 +1,31 @@
+package com.novalabsglobal.pharmacy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+
+
+    private String createdBy;
+    private String updatedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdate;
+}
---
+++ src/main/java/com/novalabsglobal/pharmacy/entity/Supplier.java
@@ -0,0 +1,32 @@
+package com.novalabsglobal.pharmacy.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class Supplier {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String contact;
+    private String website;
+
+    private String createdBy;
+    private String updatedBy;
+
+    @LastModifiedDate
+    private LocalDateTime lastUpdate;
+}
